308790c74114e0be4d3757892bb85f974276c35e
Nahir Martinez <nahirmartinez09@gmail.com>
2023-03-31 11:41:58 -0300

    segunda parte del trabajo

--- algoritmo.js
+++ algoritmo.js
@@ -1,7 +1,20 @@
-let nombre = prompt("Cual es tu nombre?");
+/*let nombre = prompt("Cual es tu nombre?");*/
+let booleano = prompt("Inserte un dato booleano:");
+let numeroEntero = prompt ("Inserte un numero entero:");
+let Caracter = prompt ("Inserte un caracter:");
+let conjuntoCaracter = prompt ("Inserte un conjunto de caracteres");
+let decimal =prompt ("Inserte un decimal");
 
+document.write("<h1> Booleano: "+ booleano + "</h1>");
+document.write ("<h2> Numero entero: " + numeroEntero + "</h2>");
+document.write ("<h3> Caracter:" + Caracter + "</h3>");
+document.write ("<h4> Conjunto de caracteres:" + conjuntoCaracter + "</h4>");
+document.write ("<h5> Numero decimal: " + decimal + "</h5>");
+
+
+/*
 console.log ( nombre);
 document.querySelector("body").innerHTML = "<h1>" +nombre + "</h1>";
-document.write("<h2>" +"Hola " + nombre + "</h2>");
+document.write("<h2>" +"Hola " + nombre + "</h2>");*/
 
 
