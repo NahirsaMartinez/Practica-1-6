6cfdc619b4d399e2093206751d848744f4d0e267
Nahir Martinez <nahirmartinez09@gmail.com>
2023-04-05 09:22:14 -0300

    Correccion mayusculas

--- algoritmo.js
+++ algoritmo.js
@@ -1,13 +1,13 @@
 /*let nombre = prompt("Cual es tu nombre?");*/
 let booleano = prompt("Inserte un dato booleano:");
 let numeroEntero = prompt ("Inserte un numero entero:");
-let Caracter = prompt ("Inserte un caracter:");
+let caracter = prompt ("Inserte un caracter:");
 let conjuntoCaracter = prompt ("Inserte un conjunto de caracteres");
 let decimal =prompt ("Inserte un decimal");
 
 document.write("<h1> Booleano: "+ booleano + "</h1>");
 document.write ("<h2> Numero entero: " + numeroEntero + "</h2>");
-document.write ("<h3> Caracter:" + Caracter + "</h3>");
+document.write ("<h3> Caracter:" + caracter + "</h3>");
 document.write ("<h4> Conjunto de caracteres:" + conjuntoCaracter + "</h4>");
 document.write ("<h5> Numero decimal: " + decimal + "</h5>");
 
